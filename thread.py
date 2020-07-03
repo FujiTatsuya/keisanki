fb64a76537af8914678e67c9409e0c009b2712d4
FujiTatsuya <67773496+FujiTatsuya@users.noreply.github.com>
2020-07-03 13:57:45 +0900

    Made it thread-safe and more cool

--- thread.py
+++ thread.py
@@ -1,30 +1,28 @@
-from threading import Thread
+from threading import Thread,RLock
+
 
 def my_function1():
-  for i in range(10)
     print("123")
     print("456")
     print("789")
 
 def my_function2():
-  for i in range(10)
     print("abc")
     print("def")
     print("ghi")
 
 def my_function3():
-  for i in range(10)
     print("ABC")
     print("DEF")
     print("GHI")
 
-if __name__ == "__main__":
-  t1 = Thread(target = my_function1)
-  t2 = Thread(target = my_function2)
-  t3 = Thread(target = my_function3)
-  t1.start()
-  t2.start()
-  t3.start()
-  t1.join()
-  t2.join()
-  t3.join()
+if __name__ == '__main__':
+    lock = RLock()
+
+    def locking_ten(f: callable):
+        for i in range(10):
+            with lock:
+                f()
+
+    func_list = [my_function1, my_function2, my_function3]
+    [Thread(target=locking_ten, args=[func]).start() for func in func_list]
